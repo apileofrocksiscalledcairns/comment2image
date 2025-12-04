4e513b664fde805b293883354325e732aa0d89dd
apileofrocksiscalledcairns <129545162+apileofrocksiscalledcairns@users.noreply.github.com>
2025-12-03 19:44:30 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
+from reddit_client import stream_comments
+from prompt_filter import extract_food_prompt
 
+
+def handle_comment(comment):
+    text = comment.body
+    prompt = extract_food_prompt(text)
+
+    if prompt:
+        print("\n==============================")
+        print("NEW FOOD PROMPT DETECTED:")
+        print(prompt)
+        print("==============================\n")
+        # to do: connect image model (not yet)
+
+
+def main():
+    for comment in stream_comments():
+        try:
+            handle_comment(comment)
+        except Exception as e:
+            print("Error processing a comeent:", e)
+
+
+if __name__ == "__main__":
+    main()
